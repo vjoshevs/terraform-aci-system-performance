9af60fc1ab8cfa37dc975a024141c0ec967c3846
Vladimir Joshevski Public <vjoshevs@cisco.com>
2023-10-25 12:50:38 +0200

    update variables for system performance

--- variables.tf
+++ variables.tf
@@ -1,31 +1,42 @@
-variable "name" {
-  description = "Tenant name."
-  type        = string
+variable "admin_state" {
+  description = "System Performance administrative state."
+  type        = bool
+  default     = false
+
+}
+
+variable "response_threshold" {
+  description = "Threshold value for response time of any requests to Nginx."
+  type        = number
+  default     = "85000"
+
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.name))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+    condition     = var.response_threshold >= 40 && var.response_threshold <= 85000
+    error_message = "Valid values are 40 to 85000."
   }
 }
 
-variable "alias" {
-  description = "Tenant alias."
-  type        = string
-  default     = ""
+
+variable "top_slowest_requests" {
+  description = "Property to set the number of slowest requests to be seen."
+  type        = number
+  default     = "5"
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.alias))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+    condition     = var.top_slowest_requests >= 1 && var.top_slowest_requests <= 10
+    error_message = "Valid values are 1 to 10."
   }
 }
 
-variable "description" {
-  description = "Tenant description."
+variable "calculation_window" {
+  description = "Window in which average time, and number of requests that go beyond the threshold is calculated."
   type        = string
-  default     = ""
+  default     = "300"
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", var.description))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
+    condition     = var.calculation_window >= 30 && var.calculation_window <= 900
+    error_message = "Frequency in which nginx is checked for avg response time in multiple of 30 sec with a maximum value of 900sec."
   }
 }
+
