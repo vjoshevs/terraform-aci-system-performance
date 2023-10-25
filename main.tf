f6a7ddf65bba29b5c748bc65e6d9c3fbd90f272c
Vladimir Joshevski Public <vjoshevs@cisco.com>
2023-10-25 17:43:32 +0200

    update main.tf file

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resource "aci_rest_managed" "fvTenant" {
-  dn         = "uni/tn-${var.name}"
-  class_name = "fvTenant"
+resource "aci_rest_managed" "commApiRespTime" {
+  dn         = "uni/fabric/comm-default/apiResp"
+  class_name = "commApiRespTime"
   content = {
-    name      = var.name
-    nameAlias = var.alias
-    descr     = var.description
+    enableCalculation = var.admin_state == true ? "enabled" : "disabled"
+    respTimeThreshold = var.response_threshold
+    topNRequests      = var.top_slowest_requests
+    calcWindow        = var.calculation_window
   }
 }
