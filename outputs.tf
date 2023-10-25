4f900d5715987da391e03235efe9b63a5761f3ca
Vladimir Joshevski Public <vjoshevs@cisco.com>
2023-10-25 13:11:21 +0200

    update output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 output "dn" {
-  value       = aci_rest_managed.fvTenant.id
-  description = "Distinguished name of `fvTenant` object."
-}
-
-output "name" {
-  value       = aci_rest_managed.fvTenant.content.name
-  description = "Tenant name."
+  value       = aci_rest_managed.commApiRespTime.id
+  description = "Distinguished name of `commApiRespTime` object."
 }
